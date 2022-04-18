09bc685fa675c2ad5feb04e844b4c4f3a8649959
FabianGal45 <fabiangal.45@gmail.com>
2022-04-18 18:06:43 +0100

    work

--- app/src/main/java/com/example/choresapp/FirebaseDatabaseHelper.java
+++ app/src/main/java/com/example/choresapp/FirebaseDatabaseHelper.java
@@ -19,6 +19,7 @@ public class FirebaseDatabaseHelper {
     private FirebaseDatabase mDatabase;
     private DatabaseReference mReference;
     private List<User> users = new ArrayList<>();
+//    private String UserID="userID";
 
     public interface DataStatus{
         void DataIsLoaded(List<User> users);
@@ -30,24 +31,31 @@ public class FirebaseDatabaseHelper {
     //UPDATE: Changing the database structure to get the usernames first then the keys
     public FirebaseDatabaseHelper(){
         mDatabase = FirebaseDatabase.getInstance("https://houseshare-2ddd0-default-rtdb.europe-west1.firebasedatabase.app/");
-        mReference = mDatabase.getReference("House/Chores");
+        mReference = mDatabase.getReference();
     }
 
     public void readData(final DataStatus dataStatus){
-        mReference.addValueEventListener(new ValueEventListener() {
+        mReference.addValueEventListener(new ValueEventListener() {//House/chores
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 users.clear();//Clears the previous users from the list to replace them with the new ones
                 System.out.println("##Clear<>");
 
-                for(DataSnapshot UserNode : snapshot.getChildren()){//Loops through all the children (users) of the chores parent in the database.
+                DataSnapshot tenantsSnapshot = snapshot.child("homes").child("homeID").child("tenants");//I made a variable so that I would not have to provide all the children each time.
+
+                for(DataSnapshot UserNode : tenantsSnapshot.getChildren()){//Loops through all the children (users) of the chores parent in the database.
                     System.out.println("### "+UserNode.getKey());
                     User user = new User();
                     PQInterface mPQ = new MyPriorityQueue();//Creates instance of the priority queue. I need a new priority queue for each user to only stores one users chores.
-                    user.setName(UserNode.getKey());//Gets the key of the reference e.g Fabian, James, Mark
+
+                    //Getting the name from the users list
+                    String userId = UserNode.getKey();//gets the user ID
+                    String userName = snapshot.child("users").child(userId).child("name").getValue(String.class);//grabs the name of the user e.g. Fabian; Mark; James.
+
+                    user.setName(userName);//Sets the user name
                     users.add(user); //Adds to the user list
 
-                    for(DataSnapshot keyNode : snapshot.child(UserNode.getKey()).getChildren()){//loops through all the child nodes of the users to grab all the chores of the user.
+                    for(DataSnapshot keyNode : tenantsSnapshot.child(UserNode.getKey()).child("chores").getChildren()){//loops through all the child nodes of the users to grab all the chores of the user.
                         if(keyNode.child("priority").exists() && keyNode.child("date").exists() && keyNode.child("date").exists()){//checks to see if the chore the necessary children. Used to prevent the app from crashing.
                             Chore chore = keyNode.getValue(Chore.class);//creates a new chore object for each user.
                             int priority = keyNode.child("priority").getValue(Integer.class);//gets the priority of the chore.
@@ -69,7 +77,7 @@ public class FirebaseDatabaseHelper {
     }
 
     public void addChore(String user, Chore chore, final DataStatus dataStatus){
-        String key = mReference.child(user).push().getKey();
+        String key = mReference.child("homes").child("homeID").child("tenants").child(user).push().getKey();
         mReference.child(user).child(key).setValue(chore).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
